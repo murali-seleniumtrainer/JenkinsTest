0acca689f7b73756ddb64ef2ab6d68cb2145c08c
murali <murali.seleniumtrainer@gmail.com>
2016-03-20 19:51:50 +0530

    updated print

--- src/com/mypackage/Demo.java
+++ src/com/mypackage/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("welcome!");
+		System.out.println("welcome to jenkins!");
 	}
 
 }
